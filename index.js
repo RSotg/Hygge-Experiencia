6f5abe2c1f860ff6bed9b597be8d5e6cbf3651ba
RaquelSoto <raquelsotoargentina@gmail.com>
2023-11-14 13:05:12 -0300

    últimos cambios

---
+++ index.js
@@ -0,0 +1,33 @@
+function validarFormulario() {
+    console.log("estoy aqui")
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var numero = document.getElementById("numero").value;
+    var email = document.getElementById("email").value;
+    var personas = document.getElementById("personas").value;
+
+    if (nombre === "" || apellido === "" || numero === "" || email === "" || personas === "") {
+        alert("Por favor, complete todos los campos y seleccione la cantidad de personas.");
+        return;
+    }
+
+    else {
+        var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!regexEmail.test(email)) {
+            alert("Por favor, ingresa un correo electrónico válido.");
+            return;
+        }
+    }
+
+    // Si todas las validaciones pasan, se envia el formulario. 
+    alert("Formulario enviado correctamente.");
+
+    //Una vez el formulario es enviado, se redirige a la sección End de la página, informando que la reserva es exitosa.
+
+    var endPage = document.getElementById("end");
+    endPage.style.display = "block";
+    window.location.href = "#end";
+
+}
+
+
